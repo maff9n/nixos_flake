1b14f6c589aec70d6c709e1eeadd39fd38b8e0e2
Marvin Mann <marvin1995mann@gmail.com>
2024-03-06 02:29:09 +0100

    replaced hard drive - freshly installed nixos

--- ullr/machine_specifics.nix
+++ ullr/machine_specifics.nix
@@ -7,10 +7,10 @@
   # networking.interfaces.enp5s0.useDHCP = true;
   # networking.interfaces.wlp3s0.useDHCP = true;
 
-  boot.loader.timeout = 10;
+  boot.loader.timeout = 5;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # boot.loader.grub.enable = true;
   # boot.loader.grub.version = 2;
   # boot.loader.grub.useOSProber = true;
@@ -20,16 +20,16 @@
 
   networking.hostName = "ullr";
 
-  nixpkgs.config.packageOverrides = pkgs: pkgs.lib.recursiveUpdate pkgs {
-    linuxKernel.kernels.linux_5_15 = pkgs.linuxKernel.kernels.linux_5_15.override {
-      extraConfig = ''
-        HAVE_LATENCYTOP_SUPPORT y
-        SCHEDSTATS y
-        LATENCYTOP y
-      '';
-      ignoreConfigErrors = true;
-    };
-  };
+  # nixpkgs.config.packageOverrides = pkgs: pkgs.lib.recursiveUpdate pkgs {
+  #   linuxKernel.kernels.linux_5_15 = pkgs.linuxKernel.kernels.linux_5_15.override {
+  #     extraConfig = ''
+  #       HAVE_LATENCYTOP_SUPPORT y
+  #       SCHEDSTATS y
+  #       LATENCYTOP y
+  #     '';
+  #     ignoreConfigErrors = true;
+  #   };
+  # };
 
     environment.systemPackages = [ pkgs.tailscale ];
 
@@ -42,5 +42,5 @@
   nixpkgs.config.allowUnfree = true;
   hardware.enableAllFirmware = true;
 
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.configurationLimit = 5;
 }
